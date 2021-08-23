2999e7bd77ea8eb7a52480ec883a7458326fec89
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2021-08-23 20:14:36 +0200

    clang-format using the latest config file

--- .tests/entrypoint_start.cpp
+++ .tests/entrypoint_start.cpp
@@ -1,15 +1,16 @@
 #include <entrypoint/start.hpp>
 #include <test/test.hpp>
 
-bool is_argc_valid(const int &argc) { return TEST(argc == 1); }
-bool is_argv_valid(char **const &argv) { return TEST(argv); }
-bool is_envp_valid(char **const &envp) { return TEST(envp); }
+bool is_argc_valid(const int& argc) { return TEST(argc == 1); }
+bool is_argv_valid(char** const& argv) { return TEST(argv); }
+bool is_envp_valid(char** const& envp) { return TEST(envp); }
 
-int main(int argc, char *argv[], char *envp[]) {
-  TEST_MAIN();
-  bool pass{true};
-  pass &= TEST_SUITE(is_argc_valid(argc));
-  pass &= TEST_SUITE(is_argv_valid(argv));
-  pass &= TEST_SUITE(is_envp_valid(envp));
-  return !pass;
+int main(int argc, char* argv[], char* envp[])
+{
+	TEST_MAIN();
+	bool pass {true};
+	pass &= TEST_SUITE(is_argc_valid(argc));
+	pass &= TEST_SUITE(is_argv_valid(argv));
+	pass &= TEST_SUITE(is_envp_valid(envp));
+	return !pass;
 }
--- fd.hpp
+++ fd.hpp
@@ -1,7 +1,8 @@
 #pragma once
-struct fd {
-  unsigned int id;
+struct fd
+{
+	unsigned int id;
 };
-static constexpr fd STDIN{0};
-static constexpr fd STDOUT{1};
-static constexpr fd STDERR{2};
+static constexpr fd STDIN {0};
+static constexpr fd STDOUT {1};
+static constexpr fd STDERR {2};
