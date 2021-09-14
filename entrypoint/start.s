1eb37a90d03c5acc8adb5485d965ef6bf9650366
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2021-09-14 21:32:27 +0200

    Fix wrong assembly instruction length

--- entrypoint/start.s
+++ entrypoint/start.s
@@ -14,7 +14,7 @@ start:
 	mov (%rsp),%rdi			   # argc
 	lea 8(%rsp),%rsi		   # argv
 	lea 8(%rsi, %rdi, 8), %rdx # envp
-	andl $0xfffffff0,%rsp	   # Align the stack to 16byte
+	andq $0xfffffffffffffff0,%rsp	   # Align the stack to 16byte
 	call main				   # Call main(argc, argv, envp)
 	mov %rax,%rdi			   # Prepare main return value before exit call
 	mov SYSCALL_EXIT,%rax	   # Load exit code
