ea3f085a3e930a27b2190ab1be8281604485d967
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2021-09-16 19:31:57 +0200

    Add test for syscall/write.hpp

---
+++ .tests/syscall_write.cpp
@@ -0,0 +1,35 @@
+#include "../fd.hpp"
+#include "../syscall/write.hpp"
+#include "../test/test.hpp"
+#include <cstdint>
+
+bool to_stdout()
+{
+	bool pass {true};
+	pass &= TEST(write("") == 1);
+	pass &= TEST(write(STDOUT, "") == 1);
+	pass &= TEST(write(STDOUT, "", 1) == 1);
+	pass &= TEST(write("A\n") == 3);
+	pass &= TEST(write(STDOUT, "B\n") == 3);
+	pass &= TEST(write(STDOUT, "C\n", 2) == 2);
+	return pass;
+}
+
+bool to_stderr()
+{
+	bool pass {true};
+	pass &= TEST(write(STDERR, "") == 1);
+	pass &= TEST(write(STDERR, "", 1) == 1);
+	pass &= TEST(write(STDERR, "B\n") == 3);
+	pass &= TEST(write(STDERR, "C\n", 2) == 2);
+	return pass;
+}
+
+extern "C" int main()
+{
+	TEST_MAIN();
+	bool pass {true};
+	pass &= TEST_SUITE(to_stdout());
+	pass &= TEST_SUITE(to_stderr());
+	return !pass;
+}
