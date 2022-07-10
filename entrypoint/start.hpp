041fe39984185ec0b082c4da58f2cca9bef0e97f
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2022-07-10 18:37:07 +0200

    Fix macOS main function name

--- entrypoint/start.hpp
+++ entrypoint/start.hpp
@@ -36,7 +36,7 @@ extern "C" [[
 	asm("lea 8(%rsp), %rsi");			   // argv
 	asm("lea 8(%rsi, %rdi, 8), %rdx");	   // envp
 	asm("andq $0xFFFFFFFFFFFFFFF0, %rsp"); // Align the stack to 16byte
-	asm("call _main");					   // Call main(argc, argv, envp)
+	asm("call main");					   // Call main(argc, argv, envp)
 	asm("mov %rax, %rdi");				   // Prepare main return value before exit call
 	asm("mov $0x2000001, %rax");		   // Load exit code
 	asm("syscall");
