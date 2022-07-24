4c9151fa8fac0cca63df782fae05a9c7e3aabed2
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2022-07-24 20:16:06 +0200

    Update syscall/write.hpp and add write overload

--- syscall/write.hpp
+++ syscall/write.hpp
@@ -3,22 +3,26 @@
 #include <stddef.h>
 #include <stdint.h>
 
-[[gnu::naked]] static int64_t sys_write(fd_t f, const char*, size_t) noexcept
+[[gnu::naked]] static int64_t sys_write(fd_t file_des, const char* buffer, size_t count) noexcept
 {
 #if __gnu_linux__ && __x86_64__
-	asm("mov $1,%rax");
+	asm("mov $1, %rax");
 	asm("syscall");
 	asm("retq");
 #elif __APPLE__ && __x86_64__
-	asm("mov $0x2000004,%rax");
+	asm("mov $0x2000004, %rax");
 	asm("syscall");
 	asm("retq");
 #elif _WIN32 && __x86_64__
-
 	asm("call WriteFile");
 #else
 	#pragma message("Unimplemented write function")
 #endif
+}
+
+[[maybe_unused]] static int64_t write(fd_t file_des, const char* buffer, size_t count) noexcept
+{
+	return sys_write(file_des, buffer, count);
 }
 
 [[maybe_unused]] static int64_t write(const char* buffer, size_t count) noexcept
