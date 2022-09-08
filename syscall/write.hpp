939cd65831c4da41cfb2ec3553c85bf56094d31c
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2022-09-08 20:20:57 +0200

    Enhance return value of write under MacOS

--- syscall/write.hpp
+++ syscall/write.hpp
@@ -13,6 +13,20 @@ write(fd_t file_des, const char* buffer, size_t count) noexcept
 #elif __APPLE__ && __x86_64__
 	asm("mov $0x2000004, %rax");
 	asm("syscall");
+	// On MacOS the errno value is not returned in %rax as a negative value (as in Linux), but a
+	// positive value is returned and the CARRY flag is set to indicate that an error occurred. To
+	// keep the behaviour consistent with Linux the value in %rax needs to be negated if the CARRY
+	// flag is set. This can be done in a few ways, the easiest being a jump that branches the flow
+	// in two different paths of execution. Using a jump might be expensive for the branch-predictor
+	// if it fails to predict the right flow. The other way to achieve this is to move the CARRY
+	// flag in a register and compute the right value in %rax only using branchless math operations.
+	// It's hard to tell beforehand which method is the most performant, therefore for the moment
+	// the easiest implementation is the one used, and the serial flow gives priority to the
+	// errorless branch.
+	asm("jc write_negate_errno");
+	asm("retq");
+	asm("write_negate_errno:");
+	asm("negq %rax");
 	asm("retq");
 #elif _WIN32 && __x86_64__
 	asm("call WriteFile");
