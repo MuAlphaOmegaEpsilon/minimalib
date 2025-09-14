a70bfd997a67dbd5479001f4b395837d745676c7
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2025-09-14 18:53:16 +0200

    Update later.hpp

--- later.hpp
+++ later.hpp
@@ -8,7 +8,7 @@ struct later_t
 	constexpr later_t(lambda_t fn) noexcept : m_fn(fn) {}
 	~later_t() noexcept { m_fn(); }
 };
-#define LATER(x) const later_t UNIQUE(later)([&](){x;})
+#define LATER const later_t UNIQUE(later) = [&]()
 
 // Copyright (C) 2024 Tommaso Bonvicini <tommasobonvicini@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
